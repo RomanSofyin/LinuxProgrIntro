99eff977a7b97a8e9978b1bf076ac133f04eeb0f
Roman Sofin <roma.sofin@gmail.com>
2020-03-09 18:22:05 +0300

    Start week1

---
+++ week1/so/solution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Функция возвращает значение длины строки string,
+ * умноженное на multiplier, и увеличивает на 1 значение,
+ * на которое указывает count.
+ */
+
+int stringStat(const char *string, size_t multiplier, int *count) {
+
+}
