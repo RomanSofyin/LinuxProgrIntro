4df605425063b1c1d94121b5a453238620c62c7a
Roman Sofin <roma.sofin@gmail.com>
2020-03-15 13:17:07 +0300

    Add libdl demo

--- week1/libdl/demo/main.c
+++ week1/libdl/demo/main.c
@@ -1,6 +1,34 @@
-void (hello_message) (const char *);
+/*
+ * This program is a demo of libdl usage (dynamic linking).
+ */
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+// hello_message is a pointer to function which is NULL
+// since it is defined in global area
+void (*hello_message) (const char *);
+
+bool init_library() {
+    // open shared object (dynamic library)
+    void *hdl = dlopen("./libHello.so", RTLD_LAZY);
+    if( NULL == hdl )
+        return false;
+    // obtain address of hello_message func
+    void * p_func = dlsym(hdl, "hello_message");
+    hello_message = (void (*)(const char*)) p_func;
+    if( NULL == hello_message)
+        return false;
+
+    return true;
+}
 
 int main() {
-    hello_message("Vasya");
+    if ( init_library() )
+        hello_message("Vasya");
+    else
+        printf("init_library failed\n");
     return 0;
 }
