27a294f652a1bf8a4ee989b2b45523342b90894d
Roman Sofin <roma.sofin@gmail.com>
2020-10-30 08:41:29 -0700

    A bit playing

--- etc/exerc1/main.c
+++ etc/exerc1/main.c
@@ -1,7 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int foo(int);
+
 int main() {
     printf("Just a custom string\n");
-    return 0;
+    int a = foo(5);
+    return 5;
+}
+
+int foo(int x) {
+    return x*123;
 }
